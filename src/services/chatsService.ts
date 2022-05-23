97f873662b255bc234b8f5b18cfcc10f6e973774
Rui Neto <ruineto11@gmail.com>
2022-05-23 02:14:51 -0300

    chore: created flow to send and load messages to an user

--- src/services/chatsService.ts
+++ src/services/chatsService.ts
@@ -1,8 +1,8 @@
 import * as errorsUtils from '../utils/errorsUtils.js';
-import * as chatUtils from "../utils/chatUtils.js";
+import * as chatUtils from "../utils/chatsUtils.js";
 import * as feedRepository from '../repositories/feedRepository.js';
 import * as userRepository from "../repositories/userRepository.js";
-import * as chatRepository from '../repositories/chatRepository.js';
+import * as chatsRepository from '../repositories/chatsRepository.js';
 
 export async function getAllContacts(userId: number) {
   if(!userId) throw errorsUtils.badRequestError('User id is Required');
@@ -13,7 +13,7 @@ export async function getAllContacts(userId: number) {
 	const follows = await feedRepository.getAllFollows(userId);
   const followsReader = follows.map((follow) => follow.followsId);
 
-  const contacts = await chatRepository.getAllContacts(followsReader);
+  const contacts = await chatsRepository.getAllContacts(followsReader);
 
   return contacts;
 }
@@ -28,7 +28,7 @@ export async function getContactsByName(userId: number, userName: string) {
   const follows = await feedRepository.getAllFollows(userId);
   const followsReader = follows.map((follow) => follow.followsId);
 
-  const contacts = await chatRepository.getAllContacts(followsReader);
+  const contacts = await chatsRepository.getAllContacts(followsReader);
   const contactsReader = contacts.map((contact) => contact.name);
   const contactsLowerReader = contacts.map((contact) => contact.name.toLowerCase());
 
@@ -41,4 +41,36 @@ export async function getContactsByName(userId: number, userName: string) {
   const filterContats = chatUtils.filterContats(contactsByName, contactsReader);
 
   return filterContats;
+}
+
+export async function getStatusById(userId: number) {
+  if(!userId) throw errorsUtils.badRequestError('User id is Required');
+
+  const user = await userRepository.findById(userId);
+  if(!user) throw errorsUtils.notFoundError("User");
+
+  const statusById = await chatsRepository.getStatusById(userId);
+
+  return statusById;
+}
+
+export async function sendMessage(messageData: chatsRepository.CreateMessageData) {
+  if(!messageData) throw errorsUtils.badRequestError('messageData is Required');
+
+  await chatsRepository.sendMessage(messageData);
+}
+
+export async function getMessages(senderName: string, recipientId: number) {
+  if(!senderName) throw errorsUtils.badRequestError('Sender name is Required');
+  if(!recipientId) throw errorsUtils.badRequestError('Recipient id is Required');
+
+  const sender = await chatsRepository.findByName(senderName);
+  if(!sender) throw errorsUtils.notFoundError("Sender");
+
+  const recipient = await userRepository.findById(recipientId);
+  if(!recipient) throw errorsUtils.notFoundError("Recipient");
+
+  const messages = await chatsRepository.getMessages(sender.id, recipientId);
+
+  return messages;
 }
