7c9ce6d4fb43e3d087d8dc31bfeba91b289314ff
Rui Neto <ruineto11@gmail.com>
2022-05-24 13:59:36 -0300

    chore: removing the enviroment variable url

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 5000;
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.APPLICATION_BASE_URL,
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
+    origin: "*",
     credentials: true
   }
 });
