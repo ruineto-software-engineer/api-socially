e43ed2beeab45c0b5e6449bfdd8e4ee217fc6840
Rui <ruineto11@gmail.com>
2023-04-25 12:11:01 -0300

    fix: add header access-control-allow-origin on app config

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,11 @@ import errorHandlingMiddleware from "./middlewares/errorHandlingMiddleware.js";
 const app = express();
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(json());
 app.use(router);
 app.use(errorHandlingMiddleware);
